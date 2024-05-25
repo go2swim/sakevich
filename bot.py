17585343783c2a1bae705fe62d353fd4b0a8a8dd
ArtemToporkov <toporkovartem259@gmail.com>
2024-05-25 16:15:27 +0500

    First valid move

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ class Bot(Client):
     def __init__(self, name: str, server_addr: tuple[str, int]):
         #сюда окно с очередью ивентов не передаём т.к тут нам рисовать окно не нужно
         super().__init__(name, server_addr, None, None)
-        # play_thread = threading.Thread(target=self.play())
-        # play_thread.start()
+        play_thread = threading.Thread(target=self.play())
+        play_thread.start()
 
     def connect(self, window: pygame.Surface = None, key_event_queue: Queue = None):
         self.socket.connect(self.server_addr)
@@ -34,11 +34,27 @@ class Bot(Client):
         print("Сделай меня, Артём")
         while(True):
             self.receive(4096)
+            move = self.find_move()
+            print(move)
             self.send({
                 "command": "move",
                 "p_name": self.name,
-                "pos_before": (1, 1),
-                "pos_after": (1, 1),
+                "pos_before": move[0],
+                "pos_after": move[1],
                 "replacement": None,
             })
+
+
+    def find_move(self):
+        for row in range(len(self.board.board)):
+            for piece in self.board.board[row]:
+                if piece is not None:
+                    if piece.valid_moves:
+                        for move in piece.valid_moves:
+                            old_location = (piece.row, piece.col)
+                            piece.move(move[0], move[1], self.board.board)
+                            self.board.update_valid_moves()
+                            return [old_location, move]
+
+
 
